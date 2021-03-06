bd8cd408872fe4b1ebacac56786af337cefd66c0
ain-crad <851056505@qq.com>
2021-03-06 16:23:54 +0800

    add parse insert and select

--- LitDatabase.cpp
+++ LitDatabase.cpp
@@ -7,13 +7,63 @@ void LitDatabase::ReadInput() {
         std::cout << "Error reading input" << std::endl;
         exit(EXIT_FAILURE);
     }
+
+    input_buffer.push_back('\0');
+    cur = &input_buffer[0];
 }
 
 void LitDatabase::Parse() {
-    if (input_buffer == ".exit") {
+    assert(cur != nullptr);
+
+    ParseWhitespace();
+    switch (*cur) {
+        case '\0': std::cout << "Empty input" << std::endl; break;
+        case '.': ParseMeta(); break;
+        default:
+            if (ParseStatement() == PARSE_STATEMENT_SUCCESS) ExecuteStatement();
+            break;
+    }
+}
+
+void LitDatabase::ParseWhitespace() {
+    assert(cur != nullptr);
+    while (*cur == ' ' || *cur == '\t' || *cur == '\n' || *cur == '\r') ++cur;
+}
+
+ParseMetaResult LitDatabase::ParseMeta() {
+    assert(cur != nullptr);
+
+    if (strcmp(cur, ".exit") == 0) {
         input_buffer.clear();
+        cur = nullptr;
         exit(EXIT_SUCCESS);
     } else {
-        std::cout << "Unrecognized command: " << input_buffer << std::endl;
+        std::cout << "Unrecognized meta command" << std::endl;
+        input_buffer.clear();
+        cur = nullptr;
+        return PARSE_META_UNRECOGNIZED;
+    }
+
+    return PARSE_META_SUCCESS;
+}
+
+ParseStatementResult LitDatabase::ParseStatement() {
+    if (strncmp(cur, "insert", 6) == 0) {
+        statement.type = STATEMENT_INSERT;
+        return PARSE_STATEMENT_SUCCESS;
+    } else if (strncmp(cur, "select", 6) == 0) {
+        statement.type = STATEMENT_SELECT;
+        return PARSE_STATEMENT_SUCCESS;
+    } else {
+        std::cout << "Unrecognized keyword" << std::endl;
+        return PARSE_STATEMENT_UNRECOGNIZED;
+    }
+}
+
+void LitDatabase::ExecuteStatement() {
+    switch (statement.type) {
+        case STATEMENT_INSERT: std::cout << "execute insert" << std::endl; break;
+        case STATEMENT_SELECT: std::cout << "execute select" << std::endl; break;
+        case STATEMENT_INVALID: std::cout << "invalid statement" << std::endl; break;
     }
 }
--- LitDatabase.h
+++ LitDatabase.h
@@ -1,17 +1,36 @@
 #ifndef LITDATABASE_H
 #define LITDATABASE_H
 
+#include <cassert>
+#include <cstring>
 #include <iostream>
 #include <string>
 
+enum ParseMetaResult { PARSE_META_SUCCESS, PARSE_META_UNRECOGNIZED };
+enum ParseStatementResult { PARSE_STATEMENT_SUCCESS, PARSE_STATEMENT_UNRECOGNIZED };
+enum StatementType { STATEMENT_INVALID, STATEMENT_INSERT, STATEMENT_SELECT };
+
+struct Statement {
+    StatementType type;
+};
+
 class LitDatabase {
 public:
+    LitDatabase() { statement.type = STATEMENT_INVALID; }
+
     void PrintPrompt();
     void ReadInput();
     void Parse();
+    void ExecuteStatement();
 
 private:
     std::string input_buffer;
+    const char* cur = nullptr;
+    Statement statement;
+
+    void ParseWhitespace();
+    ParseMetaResult ParseMeta();
+    ParseStatementResult ParseStatement();
 };
 
 #endif
